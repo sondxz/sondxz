999a2807ee5ffbf09c4a0d7aa9f7de1d14b4f721
sondxz <160985596+sondxz@users.noreply.github.com>
2024-03-22 08:18:47 +0700

    Update script.js

--- html29/script.js
+++ html29/script.js
@@ -1,3 +1,9 @@
+const search =document.querySelector('#search')
+search.addEventListener(
+    "click", function(){
+        document.querySelector('.timkiem').style.display="flex"
+    }
+)
 const login =document.querySelector('.fa-user')
 login.addEventListener(
     "click", function(){
@@ -9,5 +15,13 @@ const close =document.querySelector('.close')
 close.addEventListener(
     "click", function(){
         document.querySelector('.bgcolor').style.display="none"
+    }
+)
+
+const xemthem =document.querySelector('#xemthem')
+xemthem.addEventListener(
+    "click", function(){
+        document.querySelector('.qc2').style.display="block"
+        document.querySelector('#xemthem').style.display="none"
     }
 )
