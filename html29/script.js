ca7e813b9d78db157844a28e19b35ab06183c7cc
sondxz <160985596+sondxz@users.noreply.github.com>
2024-03-28 11:07:44 +0700

    Update script.js

--- html29/script.js
+++ html29/script.js
@@ -1,9 +1,38 @@
+/*Sản phẩm liên quan*/
+const next =document.querySelector('.fa-chevron-right')
+let index=0
+next.addEventListener(
+    "click", function(){
+        if(index<=0){
+            index=index+1
+            document.querySelector('.slider-content-left').style.right = index*98+"%"
+            document.querySelector('.fa-chevron-right').style.display = "none"
+            document.querySelector('.fa-chevron-left').style.display = "block"
+        }
+        
+    }
+);
+const prev =document.querySelector('.fa-chevron-left')
+prev.addEventListener(
+    "click", function(){
+        if(index>0){
+            index=index-1
+            document.querySelector('.slider-content-left').style.right = index*100+"%"
+            document.querySelector('.fa-chevron-right').style.display = "block"
+            document.querySelector('.fa-chevron-left').style.display = "none"
+        }
+        
+    }
+);
+
+/*Tìm kiếm*/
 const search =document.querySelector('#search')
 search.addEventListener(
     "click", function(){
         document.querySelector('.timkiem').style.display="flex"
     }
 )
+/*Đăng nhập */
 const login =document.querySelector('.fa-user')
 login.addEventListener(
     "click", function(){
@@ -17,7 +46,7 @@ close.addEventListener(
         document.querySelector('.bgcolor').style.display="none"
     }
 )
-
+/*Xem thêm */
 const xemthem =document.querySelector('#xemthem')
 xemthem.addEventListener(
     "click", function(){
@@ -25,3 +54,4 @@ xemthem.addEventListener(
         document.querySelector('#xemthem').style.display="none"
     }
 )
+
