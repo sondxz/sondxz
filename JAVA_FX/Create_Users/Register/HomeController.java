bf021c990a7b0303012396360c792321044d7083
sondxz <160985596+sondxz@users.noreply.github.com>
2024-06-04 10:33:57 +0700

    Update HomeController.java

--- JAVA_FX/Create_Users/Register/HomeController.java
+++ JAVA_FX/Create_Users/Register/HomeController.java
@@ -1,5 +1,71 @@
 package Register;
 
-public class HomeController {
+import java.awt.Label;
+import java.awt.TextField;
+import java.util.ResourceBundle;
 
+import javax.print.DocFlavor.URL;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class HomeController {
+	@FXML
+	private Label loginFullname;
+	
+	@FXML
+	private TableView<User> userListTV;
+	
+	@FXML
+	private TableColumn<User, String> emailCol;
+	
+	@FXML
+	private TableColumn<User, String> fullnameCol;
+	
+	@FXML
+	private TextField emailH;
+	
+	@FXML
+	private TextField fullnameH;
+	
+	@FXML
+	private Label Exit;
+	
+	private UserDAO um;
+	
+	private User loginUsers;
+	
+	public User getLoginUsers () {
+		return loginUsers;
+	}
+	
+	public void setLoginUsers (User loginUsers) {
+		this.loginUsers = loginUsers;
+	}
+	
+	public void initialize(URL loacation, ResourceBundle resources) {
+		Platform.runLater(() -> {
+			loginFullname.setText(loginUsers.getFullName());
+		});
+		
+		
+	}
+	@FXML
+	public void onClickExit() {
+		
+	}
+	@SuppressWarnings("unchecked")
+	public void onClickAdd () {
+		
+	}
+	@SuppressWarnings("unchecked")
+	public void onClickFix() {
+
+	}
+	@SuppressWarnings("unchecked")
+	public void onClickDel() {
+
+	}
 }
