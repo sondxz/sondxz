90920f236cf602eba0153e13a418f9eef2be9831
sondxz <160985596+sondxz@users.noreply.github.com>
2024-06-06 22:22:48 +0700

    Update RegisterController.java

--- JAVA_FX/Create_Users/Register/RegisterController.java
+++ JAVA_FX/Create_Users/Register/RegisterController.java
@@ -1,15 +1,68 @@
 package Register;
 
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class RegisterController {
 	@FXML
-	private TextField EmailTF,FullnameTF;
-	
+	TextField EmailTF;
 	@FXML
-	public void show() {
-		
+	TextField FullnameTF;
+	@FXML
+	Label msg;
+
+	@FXML
+	public void onClickRegisterButton() {
+		System.out.println("email" + ": " + EmailTF.getText());
+		System.out.println("name" + ": " + FullnameTF.getText());
+
+		User user = UserDAO.checkUser(EmailTF.getText());
+
+		if (user != null) {
+			msg.setText("cant sign up, your email was existed");
+			System.out.println(user.toString());
+		} else {
+
+//			// add vao csdl
+//			userDAO.addUser(new User(email.getText(), name.getText()));
+//			// đóng cửa sổ hiện tại
+			EmailTF.getScene().getWindow().hide();
+//			// mở cứa sổ home
+
+			goHomeScreen();
+
+		}
+
 	}
-	
+
+	private void goHomeScreen() {
+
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+
+			// thêm layout vào scene
+			Scene scene = new Scene(root);
+
+			// thêm scene vào stage
+			Stage homeStage = new Stage();
+			homeStage.setScene(scene);
+			homeStage.setTitle("Home");
+
+			// hiển thị stage
+			homeStage.show();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Không hiển thị menu được");
+		}
+
+	}
+
 }
